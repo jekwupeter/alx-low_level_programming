4f06832b332a67069cec8cf1c99eae8944c3db24
root <you@example.com>
2022-03-10 17:16:39 -0800

    making code correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/**                                                                              * main - Prints a quote            
+ *
+ * Return: Always 0 (Success)                                             
+ */ 
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle");
-
+		puts("\"Programming is like building a multilingual puzzle");
 	        return (0);
 }
