23b290915c01d5c22a903f3731ff2a025a54be23
root <you@example.com>
2022-03-16 20:22:33 -0700

    correction for last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,10 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
+	if (n < 0){
 		n = n * -1;
-	_putchar((n % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
 	else
 		_putchar((n % 10) +'0');
 	return (n % 10);
