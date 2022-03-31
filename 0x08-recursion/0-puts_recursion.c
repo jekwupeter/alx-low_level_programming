2685775872d0d1b8c5356309ed1687ca2acdea59
root <you@example.com>
2022-03-30 22:18:48 -0700

    Task upload

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion -prints a string with recursion
+ * @s:string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 10)
+	{
+		_putchar(0);
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
