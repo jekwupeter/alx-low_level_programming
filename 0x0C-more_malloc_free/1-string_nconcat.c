e849c4e707e6f00f6cbd9430f8f28aaaf9b05657
jekwupeter <cephaspeter123@gmail.com>
2022-04-08 06:59:31 +0000

    Task correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new_string;
 	unsigned int i, j, index, count;
 
-	i = j = count = index = 0;
-
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 	for (i = 0; s1[i] != 0; i++)
 		;
 	for (j = 0; s2[j] != 0; j++)
