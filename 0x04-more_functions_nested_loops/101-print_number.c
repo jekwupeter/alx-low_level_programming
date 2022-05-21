3b8796ee04797ba2b03a807a2a930d3be3589c73
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-05-21 13:34:37 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,9 +7,9 @@
  */
 void print_number(int n)
 {
-	double m; /* power of 10 */
+	long int m; /* power of 10 */
 	int c; /* boolean check */
-	double num; /* convert int to long */
+	long int num; /* convert int to long */
 
 	num = n;
 	/* negatives */
