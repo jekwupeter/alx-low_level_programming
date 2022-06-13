b70967af846765fef12eb7205580c8fdefea150d
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-13 06:32:32 +0100

    Task upload

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds a node in a dlinked list.
+ * @head: head of the dlinked list.
+ * @index: index of node to find
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the address of the node to find.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int idx = 0;
+
+    	while(head)
+	{
+		head = head->next;
+        	idx++;
+        	if (idx == index)
+			return (head);
+    }
+}
