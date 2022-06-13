1ced061738671211d27d5e46f510c02e794f128c
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-13 06:38:43 +0100

    correction

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,9 +13,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
         int idx = 0;
         while(head)
         {
-                head = head->next;
-                idx++;
                 if (idx == index)
                         return (head);
-    }
+                head = head->next;
+                idx++;
+        }
+        return (NULL);
 }
