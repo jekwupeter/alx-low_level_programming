0ff4d6875cdd1dea83da5669251f8fa68280bc7b
jekwupeter <cephaspeter123@gmail.com>
2022-06-14 17:10:41 +0000

    Task upload

---
+++ 0x18-dynamic_libraries/funcs/0-add.c
@@ -0,0 +1,60 @@
+/**
+ * add - adds two numbers
+ * @a: first input number
+ * @b: second input number
+ *
+ * Return: sum of two digits
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - finds the difference btw two numbers
+ * @a: first input number
+ * @b: second input number
+ *
+ * Return: difference of two digits
+ *
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * mul - multiplies two numbers
+ * @a: first input number
+ * @b: second input number
+ *
+ * Return: multiplication result of two digits
+ *
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * div - divides two numbers
+ * @a: first input number
+ * @b: second input number
+ *
+ * Return: division of two digits
+ *
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mod - finds modulus two numbers
+ * @a: first input number
+ * @b: second input number
+ *
+ * Return: modulus of two digits
+ *
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
